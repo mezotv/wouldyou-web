693b6d0a72a6b5a8ef91c43760aac81f8daa5dce
Dominik K <dominik@koch-bautechnik.de>
2022-09-11 17:34:41 +0200

    Added redirects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,26 @@ module.exports = async (phase, { defaultConfig }) => {
   const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    async redirects() {
+      return [
+        {
+          source: '/vote',
+          destination: 'https://top.gg/bot/981649513427111957/vote',
+          permanent: true,
+        },
+        {
+          source: '/invite',
+          destination: 'https://discord.com/oauth2/authorize?client_id=981649513427111957&permissions=274878377024&scope=bot%20applications.commands',
+          permanent: true,
+        },
+        {
+          source: '/support',
+          destination: 'https://discord.gg/wouldyou',
+          permanent: true,
+        },
+      ]
+    },
   }
-  return nextConfig
+  return nextConfig;
+
 }
